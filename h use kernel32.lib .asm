eca3bcd9d92ba000cc127147780239e255d32ca7
Dis <120120666+DisplaybookGithub@users.noreply.github.com>
2024-04-01 20:01:54 -0400

    Add files via upload

---
+++ h use kernel32.lib .asm
@@ -0,0 +1,22 @@
+default rel
+extern GetStdHandle, WriteConsoleA
+
+section .data
+    message db 'HelloWorld', 0Ah
+    message_len equ $ - message
+
+section .bss
+    bytes_written resd 1
+
+section .text
+    global main
+    main:
+        sub rsp, 8
+        mov rcx, -11
+        call GetStdHandle
+        mov rcx, rax
+        lea rdx, [message]
+        mov r8d, message_len
+        lea r9, [bytes_written]
+        call WriteConsoleA
+        
